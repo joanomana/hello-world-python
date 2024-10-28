9b0bbefd321512ce9d80aac80744d65ba5679a80
joanomana <joansebastianomasu@gmail.com>
2024-10-28 12:43:18 -0500

    Las 3 partes principales de un algoritmo usando Python

--- main.py
+++ main.py
@@ -2,4 +2,18 @@ name, age, country = input("Enter your name, age, country: ").split(' ')
 words = (f"Hello {name}, are you {age} years old?, what can you tell me about {country} " )
 print(words)
 
+ ###########Types of data############
+ ##str = string
+word = "Word"
+##int = integer
+number = 4
+##float = decimal
+decimal = 4.8
+##boolean = True or False
+married = True
+print(f"Are you married? {married}")
+
+promedio = float(input("Ingrese un promedio: ")) #Its important to define the type of data that the input will be
+print(type(promedio))
+
 
