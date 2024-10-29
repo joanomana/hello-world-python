feb018ace411198de0509767d85d92bda916fced
joanomana <joansebastianomasu@gmail.com>
2024-10-29 11:30:35 -0500

    Update

--- main.py
+++ main.py
@@ -16,4 +16,24 @@ print(f"Are you married? {married}")
 promedio = float(input("Ingrese un promedio: ")) #Its important to define the type of data that the input will be
 print(type(promedio))
 
+#dictionary
+address = {"Country":"Mexico", "City":"CDMX", "ZipCode": 12345}
+print(address)
+print(type(address))
 
+#list
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(type(fruits))
+
+#tuple
+colors = ("red", "blue", "green")
+print(colors)
+print(type(colors))
+
+#set
+animals = {"dog", "cat", "bird"}
+print(animals)
+print(type(animals))
+###
+mundo="Tierra"
